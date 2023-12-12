f186e14860902915e2b39bd9150a7ff4959ebb85
balaguer0 <carlos@1DAW-28>
2023-12-12 12:52:52 +0100

    for

--- src/project/hola.java
+++ src/project/hola.java
@@ -5,7 +5,10 @@ public class hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");
-
+		for(int i=0;i<13;i++) {
+			System.out.println(i+1);
+		}
+			
 	}
 
 }
