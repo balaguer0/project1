b4a559caccd1d9fcfcd1b77e0a18b19f8dfdbcae
Angela <angela452001@outlook.es>
2023-12-12 13:51:39 +0100

    area y perimetro Circunferencia

--- src/project1/ArPerCircunferencia.java
+++ src/project1/ArPerCircunferencia.java
@@ -1,10 +1,25 @@
 package project1;
 
+import java.util.Scanner;
+
 public class ArPerCircunferencia {
 
 	public static void main(String[] args) {
-		System.out.println("DNHGDGNHODG");
+		Scanner entrada=new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la circunferencia");
+		radio=entrada.nextInt();
+		System.out.println("El area de la circunferencia es: "+AreaCircunferencia(radio));
+		System.out.println("El perimetro de la circunferencia es:"+PerimetroCircunferencia(radio));
 
+	}
+	public static double AreaCircunferencia(int radio) {
+		double pi=3.1416;
+		return radio*radio*pi;
+	}
+	public static double PerimetroCircunferencia(int radio) {
+		double pi=3.1416;
+		return (radio+radio)*pi;
 	}
 
 }
